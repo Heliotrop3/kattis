6abebbe338551342b4c5cd91d8cc75e8c7d42dd8
T <43817374+tyler-huffman@users.noreply.github.com>
2020-03-08 18:35:28 -0400

    Difficulty: Trivial

---
+++ Take Two Stones/TakeTwoStones.cpp
@@ -0,0 +1,16 @@
+/*
+Tyler Huffman
+2020-08-03
+https://open.kattis.com/problems/twostones
+*/
+#include <iostream>
+
+int main(){
+	// Create integer to hold input
+	int value = 0;
+	// Grab input
+	std::cin >> value;
+	// If the number is divisible by 2 display Bob otherwise display Alice
+	(std::cout << (( value % 2 == 0) ? "Bob" : "Alice"));
+	return 0;
+}
