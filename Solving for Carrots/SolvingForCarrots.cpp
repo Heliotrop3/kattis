b0067e5fdee310ebb233c47ab243e6d8f793ae5f
T <43817374+tyler-huffman@users.noreply.github.com>
2020-03-08 23:31:02 -0400

    Difficulty: Trivial

---
+++ Solving for Carrots/SolvingForCarrots.cpp
@@ -0,0 +1,16 @@
+/*
+ Tyler Huffman
+ 2020-03-08
+ https://open.kattis.com/problems/carrots
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x;                // Declare the integer to hold the number of huffle-puff problems solved
+	cin.ignore(256,' ');  // Ignore the number of contestants
+	cin >> x;             // Grab the number of huffle-puff problems solved
+	cout << x << endl;    // Display the number of huffle-puff problems
+	
+	return 0;
+}
