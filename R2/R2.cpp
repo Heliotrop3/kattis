aed70b45cf40597a39166ae20efb90ecba37a9f6
T <43817374+tyler-huffman@users.noreply.github.com>
2020-03-08 23:26:30 -0400

    Difficulty: Trivial

--- R2/R2.cpp
+++ R2/R2.cpp
@@ -8,8 +8,7 @@ using namespace std;
 
 int main(){
 	int r1,s;                      // Create the integer variables to hold R1 and S respectively
-	cin >> r1;                     // Read in R1
-	cin >> s;                      // Read in S
+	cin >> r1 >> s;                // Read in R1 and S
 	cout << ((2*s) - r1) << endl;  // S=(R1 + R2)/2 => R2 = ((2 * S)-R1). Plug in R1 and S to solve for R2
 	
 	return 0;
