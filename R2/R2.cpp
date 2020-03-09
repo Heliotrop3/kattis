d8d992b019d8e3f803f6c177c9d263bcf8e448c7
T <43817374+tyler-huffman@users.noreply.github.com>
2020-03-08 23:17:15 -0400

    Difficulty: Trivial

---
+++ R2/R2.cpp
@@ -0,0 +1,16 @@
+/*
+ Tyler Huffman
+ 2020-03-08
+ https://open.kattis.com/problems/r2
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+	int r1,s;                      // Create the integer variables to hold R1 and S respectively
+	cin >> r1;                     // Read in R1
+	cin >> s;                      // Read in S
+	cout << ((2*s) - r1) << endl;  // S=(R1 + R2)/2 => R2 = ((2 * S)-R1). Plug in R1 and S to solve for R2
+	
+	return 0;
+}
